bd4d42d147463cdc912a224af13adc13cc893e67
hanszimmer <michael.weitnau@live.de>
2020-04-05 10:44:50 +0200

    add verion to seperate file

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+source current_version
 docker build \
     -f Dockerfile \
     -t mn \
     -t hanszimmer5000/mininet:v2.2.2-latest \
-    -t hanszimmer5000/mininet:v2.2.2-v1.1 \
+    -t hanszimmer5000/mininet:$current_version \
     .
---
+++ push.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+source current_version
+docker push hanszimmer5000/mininet:$current_version
