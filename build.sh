937237eaf6efd8e6707e060a44b720b8479ac0c7
hanszimmer <michael.weitnau@live.de>
2020-04-05 11:08:12 +0200

    change image version

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ source ./current_version
 docker build \
     -f Dockerfile \
     -t mn \
-    -t hanszimmer5000/mininet:v2.2.2-latest \
+    -t hanszimmer5000/mininet:latest \
     -t hanszimmer5000/mininet:$current_version \
     .
