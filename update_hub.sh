6d17dc93d451de5f04c159be5679305509ceaff8
hanszimmer <michael.weitnau@live.de>
2020-04-07 15:37:30 +0200

    fix version and add branch

--- update_hub.sh
+++ update_hub.sh
@@ -5,8 +5,13 @@ git checkout v2.2
 git merge master
 git push
 
-# This will trigger a build for a ":v2.2-latest" and ":latest" image build
+# This will trigger a build for a ":v2.3-latest" and ":latest" image build
 git checkout v2.3
+git merge master
+git push
+
+# This will trigger a build for a ":buster-v2.3-latest" image build
+git checkout buster-v2.2
 git merge master
 git push
 
