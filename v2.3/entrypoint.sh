ef01f327b6b5a0f7b4a1f7de92bde00fb5bf74e2
hanszimmer <michael.weitnau@live.de>
2020-04-09 14:52:34 +0200

    execute commands right and update version number

--- v2.3/entrypoint.sh
+++ v2.3/entrypoint.sh
@@ -7,7 +7,7 @@ service openvswitch-switch start >/dev/null #This command may cause errors that 
 ovs-vsctl set-manager ptcp:6640 1>/dev/null
 
 echo "-- Got Command and Arguments: '$@'"
-exec "$@"
+"$@"
 
 echo "-- Stopping OVS"
 service openvswitch-switch stop 1>/dev/null
